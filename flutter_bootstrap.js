7d2834ead98c71c33efc09bf212d2fdb8b379528
prateek singh <prateeksingh87951@gmail.com>
2025-11-29 15:44:24 +0530

    Add files via upload
    small changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "8211758"
+    serviceWorkerVersion: "2503108295"
   }
 });
